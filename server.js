b64c050cc6f6360ce4cd51ba7cb91b480be6fdd2
mfeke <mfekethohulane@gail.com>
2025-05-18 10:56:16 +0200

    deploting to vercel

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
-// const bodyParser = require("body-parser")
+const bodyParser = require("body-parser")
 const express = require("express")
-const app = express()
-// const cors = require("cors")
-// const userRouter = require("./routes/user.route")
-// const categoryRouter = require("./routes/category.route")
+const app = express ()
+const cors = require("cors")
+const userRouter = require("./routes/user.route")
+const categoryRouter = require("./routes/category.route")
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended:true
 }));
 
 
 
-app.get("/", (req, res) => {
-  res.json({ info: "Server Online" })
+app.get("/" , (req , res) =>{
+    res.json({info: "Server Online"})
 })
-const PORT = 2022
-// Cross Orign Resource sharing
+ const PORT = 2022
+ // Cross Orign Resource sharing
 
 
-app.use(cors());
+ app.use(cors());
 
-// const db = require("./models");
-// db.mongoose.set('strictQuery', true);
+ const db = require("./models");
+  db.mongoose.set('strictQuery', true);
 
-//  db.mongoose
-//    .connect(db.url, {
-//      useNewUrlParser: true,
-//      useUnifiedTopology: true
-//    })
-//    .then(() => {
-//      console.log("Connected to the database!");
-//    })
-//    .catch(err => {
-//      console.log("Cannot connect to the database!", err);
-//      process.exit();
-//    });
+ db.mongoose
+   .connect(db.url, {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+   })
+   .then(() => {
+     console.log("Connected to the database!");
+   })
+   .catch(err => {
+     console.log("Cannot connect to the database!", err);
+     process.exit();
+   });
 
-//    app.use('/api/auth', userRouter)
-//       app.use('/api/app', categoryRouter)
+   app.use('/api/auth', userRouter)
+      app.use('/api/app', categoryRouter)
 
 
-app.listen(PORT, () => {
-  console.log(`Server running at port:${PORT}`)
+app.listen(PORT, ()=> {
+    console.log(`Server running at port:${PORT}`)
 }) 
