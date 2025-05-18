f82fe80a4ac64e793b818bb8463101c1399595d9
mfeke <mfekethohulane@gail.com>
2025-05-18 11:06:28 +0200

    deploting to vercel

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT 
+const bodyParser = require("body-parser")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const userRouter = require("./routes/user.route")
+const categoryRouter = require("./routes/category.route")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/', (req, res) => {
-  res.send('Hello from Vercel!');
-});
+
+
+app.get("/", (req, res) => {
+  res.json({ info: "Server Online" })
+})
+const PORT = 2022
+// Cross Orign Resource sharing
+
+
+app.use(cors());
+
+const db = require("./models");
+db.mongoose.set('strictQuery', true);
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+app.use('/api/auth', userRouter)
+app.use('/api/app', categoryRouter)
+
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
-module.exports = app;
+  console.log(`Server running at port:${PORT}`)
+}) 
