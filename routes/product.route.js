ff002afa8f0e06ead2392ae464c3133d86ed2c35
mfeke <mfekethohulane@gail.com>
2025-06-19 08:44:26 +0200

    update database

--- routes/product.route.js
+++ routes/product.route.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/product.controllers");
 
 // Define routes
 router.get("/getAllProduct", controller.getAllProduct)
-router.get("/getProductById/:id", controller.getProductById)
+// router.get("/getProductById/:id", controller.getProductById)
 
 module.exports = router; // Export the router
 
