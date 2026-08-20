3b37c7d0c9a3090b75fdcdb8147c691e0bf51c2d
Lindiwe02 <132814659+Lindiwe02@users.noreply.github.com>
2026-08-20 04:11:25 +0000

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcryptjs");
-const Customer = require("../models/user.model");
+const User = require("../models/user.model");
 
 const authConfig = require("../db/auth.config")
 
@@ -12,24 +12,20 @@ exports.signup = (req, res) => {
         "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
     });
 
-    const { username, email, address, phone, password } = req.body;
+    const { email } = req.body
 
-    if (!username || !email ||!password) {
+    if (!email ) {
         return res.send(400).json({ message: "Missing values" })
     }
 
-    Customer.findOne({ email }).then((userFound) => {
+    User.findOne({ email }).then((userFound) => {
         if (!userFound) {
-            const user = new Customer({
-                username,
-                email,
-                phone,
-                address,
-                password: bcrypt.hashSync(req.body.password, 8)
+            const user = new User({
+                email, 
             });
 
             user.save().then(message => {
-                return res.status(200).json({ message: "Account Created" });
+                return res.status(200).json({ message: "Account Created"  });
 
             }).catch(err => {
                 return res.status(300).json(err);
@@ -44,49 +40,3 @@ exports.signup = (req, res) => {
 };
 
 
-exports.signin = (req, res) => {
-
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-        return res.send(401).json({ message: "Missing values" })
-    }
-
-    Customer.findOne({ email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ message: "User Not found" });
-            }
-
-            const passwordIsValid = bcrypt.compareSync(
-                req.body.password,
-                user.password
-            );
-
-            if (!passwordIsValid) {
-                return res.status(401).json({ message: "Invalid Password!" });
-            }
-
-            const token = jwt.sign(
-                { id: user.id },
-                authConfig.secret,
-                { expiresIn: "2h" }
-            );
-
-            res.status(200).json({
-                id: user._id,
-                username: user.username,
-                email: user.email,
-                role: user.role,
-                image: user.image,
-                phone: user.phone,
-                address: user.address,
-                accessToken: token
-            });
-        })
-        .catch(err => {
-            return res.status(500).send({ message: err.message || "Some error occurred while signing in." });
-        });
-
-
-};
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,8 +5,7 @@ const router = express.Router();
 const controller = require("../controllers/user.controller");
 
 // Define routes
-router.post("/signup", verifySignUp.checkDuplicateUsernameOrEmail,controller.signup);
-router.post("/signin", controller.signin);
+router.post("/signup",controller.signup);
 
 
 module.exports = router; // Export the router
