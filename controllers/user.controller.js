2ac449693e6d80e966e648c0ffe35656666dade4
jnoha501-gif <jnoha501@gmail.com>
2026-08-23 09:55:51 +0000

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -69,6 +69,21 @@ exports.signup = async (req, res) => {
 }
 }
 
+exports.getProfile = async (req, res) =>{
+
+    try {
+
+        const { userId} = req
+        let user = await User.findOne({_id:userId})
+        res.status(200).json(user)
+        
+    } catch (err) {
+        res.status(500).json({message: err.message})
+        
+    }
+    
+}
+
 
 
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,12 @@
 const { verifySignUp } = require("../middleware");
-
+const { authJwt } = require("../middleware");
 const express = require('express');
 const router = express.Router();
 const controller = require("../controllers/user.controller");
 
 // Define routes
 router.post("/signup",controller.signup);
-
+router.get("/getUser",authJwt.verifyToken, controller.getProfile)
 
 module.exports = router; // Export the router
 
